e9ff347e37e041cdb50b13784f0fa62ac83f1be5
wesenbergg <b.b.j.1998@gmail.com>
2020-04-07 22:37:12 +0300

    threads from 8 to 4

--- SuMa11-Memento/src/memento/Main.java
+++ SuMa11-Memento/src/memento/Main.java
@@ -3,8 +3,8 @@ package memento;
 public class Main {
 
 	public static void main(String[] args) {
-		int n = 8; // Number of threads 
-        for (int i=0; i<n; i++) 
+		int n = 4; // Number of threads 
+        for (int i=1; i<=n; i++) 
         { 
         	Arvuri arvuri = new Arvuri(i+"");
             Thread object = new Thread(arvuri); 
